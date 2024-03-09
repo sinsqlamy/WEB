701145641ff184128acb36dedc90cf855a2260c9
Denis <ds19092004@gmail.com>
2024-03-09 23:03:44 +0200

    feat: do 2 task -(Sat, Mar 09, 2024 11:03:44 PM)-

--- lab_2/code/index.php
+++ lab_2/code/index.php
@@ -10,4 +10,19 @@ $order .= " additional string"; // Concatenate a string using the concatenation 
 // Print the value of the variable $very_bad_unclear_name
 echo $very_bad_unclear_name;
 
+// 2
+$integer_variable = 10;
+echo $integer_variable;
+
+echo "\n";
+
+$floating_variable = 5.5;
+echo $floating_variable;
+
+echo 10 + 2;
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+$expenses_difference = $last_month - $this_month;
+echo "\nExpenses difference: " . $expenses_difference;
 ?>
